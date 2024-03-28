961fadb6306d86c4fd8b48f003d4c23f60336f42
shyunyy01 <165003061+shyunyy01@users.noreply.github.com>
2024-03-28 04:56:42 +0000

    all

--- batch_prediction.sh
+++ batch_prediction.sh
@@ -2,3 +2,6 @@ BASH_ENV=~/.bashrc
 ROOT_PATH=/workspaces/lgcns-mlops-practice
 
 $ROOT_PATH/.venv/bin/python $ROOT_PATH/batch_prediction.py >> $ROOT_PATH/cron.log 2>&1
+
+
+Thu Mar 28 02:22:51 UTC 2024
