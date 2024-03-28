fe43da9e25c2baaee6926aea66e101e2d9123254
shyunyy01 <165003061+shyunyy01@users.noreply.github.com>
2024-03-28 02:04:07 +0000

    Modify train.py

--- train.py
+++ train.py
@@ -1,5 +1,3 @@
-# TODO: 적절한 위치에 맞는 수준으로 로그 출력되도록 코드 작성
-
 # sourcery skip: raise-specific-error
 import os
 import sys
@@ -23,32 +21,32 @@ from src.common.metrics import rmse_cv_score
 from src.common.utils import get_param_set
 from src.preprocess import preprocess_pipeline
 
-
-# 로그 들어갈 위치 
-# TODO: 로그를 정해진 로그 경로에 logs.log로 저장하도록 설정
-
-sys.excepthook = handle_exception
+# 로그 들어갈 위치
+# 로그를 정해진 로그 경로에 logs.log로 저장하도록 설정
+logger = set_logger(os.path.join(LOG_FILEPATH, "logs.log"))
+sys.excepthook = handle_exception  # 전체 코드에 대해서 에러를 잡고 싶을 때(logger.py)
 warnings.filterwarnings(action="ignore")
-
 
 
 if __name__ == "__main__":
     train_df = pd.read_csv(os.path.join(DATA_PATH, "house_rent_train.csv"))
+    logger.debug("Load data...")
 
     _X = train_df.drop(["rent", "area_locality", "posted_on"], axis=1)
     y = np.log1p(train_df["rent"])
-    
-    # TODO: X=_X, y=y로 전처리 파이프라인을 적용해 X에 저장
+
+    # X=_X, y=y로 전처리 파이프라인을 적용해 X에 저장
+    X = preprocess_pipeline.fit_transform(X=_X, y=y)
 
     # Data storage - 피처 데이터 저장
     if not os.path.exists(os.path.join(DATA_PATH, "storage")):
         os.makedirs(os.path.join(DATA_PATH, "storage"))
     X.assign(rent=y).to_csv(
-        # TODO: DATA_PATH 밑에 storage 폴더 밑에 피처 데이터를 저장
-        
+        # DATA_PATH 밑에 storage 폴더 밑에 피처 데이터를 저장
+        os.path.join(DATA_PATH, "storage", "housr_rent_train_features.csv"),
         index=False,
     )
-
+    logger.debug("Save the feature data...")
 
     params_candidates = {
         "learning_rate": [0.01, 0.05, 0.1],
@@ -59,25 +57,33 @@ if __name__ == "__main__":
     param_set = get_param_set(params=params_candidates)
 
     # Set experiment name for mlflow
-    experiment_name = "new_experiment"
+    logger.debug("Set an experiment for mlflow..")
+    # experiment_name = "new_experiment"
+    experiment_name = "new_experiment_with_log"  ## 소현: 로그 수정 후
     mlflow.set_experiment(experiment_name=experiment_name)
     mlflow.set_tracking_uri("./mlruns")
 
     for i, params in enumerate(param_set):
-
         run_name = f"Run {i}"
+        logger.info(f"{run_name}: {params}")
+
         with mlflow.start_run(run_name=f"Run {i}"):
             regr = GradientBoostingRegressor(**params)
             # 전처리 이후 모델 순서로 파이프라인 작성
             pipeline = Pipeline(
-                # TODO: 전처리 파이프라인와 모델을 파이프라인으로 묶을 것
+                # 전처리 파이프라인와 모델을 파이프라인으로 묶을 것
+                [("preprocessor", preprocess_pipeline), ("regr", regr)]
             )
             pipeline.fit(_X, y)
 
             # get evaluations scores
             score_cv = rmse_cv_score(regr, X, y)
+            logger.info(
+                "Cross-Validation RMSE score for the current run"
+                f"{i}: {score_cv.mean(): 4f} (std = {score_cv.std(): 4f})"
+            )
 
-            name = regr.__class__.__name__
+            name = regr.__class__.__name__  ## 소현: GradientBoostingRegressor
             mlflow.set_tag("estimator_name", name)
 
             # 로깅 정보 : 파라미터 정보
@@ -86,7 +92,8 @@ if __name__ == "__main__":
             # 로깅 정보: 평가 메트릭
             mlflow.log_metrics(
                 {
-                    "RMSE_CV": #TODO: RMSE_CV 라는 이름으로 score_cv.mean()을 저장
+                    # RMSE_CV 라는 이름으로 score_cv.mean()을 저장
+                    "RMSE_CV": score_cv.mean()
                 }
             )
 
@@ -96,14 +103,12 @@ if __name__ == "__main__":
 
             # 모델 아티팩트 저장
             mlflow.sklearn.log_model(
-                # TODO: 최종 파이프라인을 저장
+                pipeline,
                 "model",
             )
 
             # log charts
-            mlflow.log_artifact(
-                # TODO: 아티팩트 경로 설정
-            )
+            mlflow.log_artifact(ARTIFACT_PATH)  # 아티팩트 경로 설정
 
             # generate a chart for feature importance
             log_feature_importance(train=X, model=regr)
@@ -118,22 +123,17 @@ if __name__ == "__main__":
 
     best_run = mlflow.get_run(best_run_df.at[0, "run_id"])
     best_params = best_run.data.params
+    logger.info(f"Best Hyper-params: {best_params}")
 
     best_model_uri = f"{best_run.info.artifact_uri}/model"
 
-    # TODO: 베스트 모델을 아티팩트 폴더에 복사
-    copy_tree(
-              # TODO: 베스트 모델 URI에서 file:// 를 지울 것, 
-              ARTIFACT_PATH
-    )
-
+    # 베스트 모델을 아티팩트 폴더에 복사
+    copy_tree(best_model_uri.replace("file://", ""), ARTIFACT_PATH)
 
     # BentoML에 모델 저장
     bentoml.sklearn.save_model(
         name="house_rent",
-        model=mlflow.sklearn.load_model(
-            # TODO: 베스트 모델 URI
-        ),
+        model=mlflow.sklearn.load_model(best_model_uri),  # 베스트 모델 URI
         signatures={"predict": {"batchable": True, "batch_dim": 0}},
         metadata=best_params,
     )
